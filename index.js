21f8e3f1bb9286c0d6f1a4aa23cf1185504f79fb
Pavel Barros Quintanilla <pavelbarrosq@gmail.com>
2021-04-01 15:49:18 +0200

    checkIn progress..

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let loginButton = document.getElementById("loginButton");
 let cartButton = document.getElementById("cartButton");
-let productRow = document.getElementById("productRow");
+let pr = document.getElementById("productRow");
 let products;
 getProducts();
 
@@ -13,25 +13,22 @@ function cartButtonEvent() {
 }
 
 function getProducts() {
-    fetch('ProduktAPI.json')
+    fetch('ProductAPI.json')
     .then((response) => response.json())
     .then(function(data) {
         products = data;
 
-        return products.map(function(product) {
-            let name = product.name;
-            let image = product.image;
-            let price = product.price;
-
-            populateColumns(name, image, price);
-        })
+        products.forEach(e => {
+            populateColumns(e.name, e.image, e.price);
+            pr.innerHTML = '<div class="col-sm-3"><div><img class="mw-100 h-100" src="'+e.image+'"alt=""></div><div><p>'+e.price+'$</p></div><div><p>'+e.name+'</p></div><div><button type="button" class="btn btn-secondary btn-block">Hola</button></div></div>';
+            console.log(e.name);
+        });
     })
 }
 
+
 function populateColumns(name, image, price) {
-    productRow.append('<div class="col-sm-3"><div><img class="mw-100 h-100" src="'+image+'"alt=""></div><div><p>'+price+'</p></div><div><p>'+name+'</p></div><div><button type="button" class="btn btn-secondary btn-block">Hola</button></div></div>');
+    pr.innerHTML = '<div class="col-sm-3"><div><img class="mw-100 h-100" src="'+image+'"alt=""></div><div><p>'+price+'$</p></div><div><p>'+name+'</p></div><div><button type="button" class="btn btn-secondary btn-block">Hola</button></div></div>';
 
-
-   /*  $prow.append('<div class="col-sm-3"><div class="panel panel-primary"><div class="panel-heading" id="product-title">'+title+'</div><div class="panel-body"><img src='+image+' class="img-responsive" style="width:100%" alt="Image"></div><div class="panel-body"><p id="price">'+price+' $</p><p id="description">'+description+'</p></div><div class="panel-footer"><button type="button" class="btn btn-success btn-block" id='+id+'>Buy</button></div></div></div>');  */
 }
 
