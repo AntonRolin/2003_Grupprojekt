87667cefe407b87f37c3df5d17ca22e1196c9566
Anton Rolin <antonrolin@hotmail.com>
2021-04-01 15:55:22 +0200

    redid cart from scratch

--- README.md
+++
@@ -1,8 +0,0 @@
-# 2003_Grupprojekt
-
-gh-pages branchen ska endast ha HTML/CSS & JavaScript filer
-
-Hemsidan publiceras på https://hakim-livs.netlify.app/
-
-Alternativ länk: https://antonrolin.github.io/2003_Grupprojekt/
-
---
+++ cart.js
@@ -0,0 +1,58 @@
+const totalPriceLabel = document.getElementById('totalPrice');
+const tbody = document.getElementById('productCartTable');
+
+let productsInCart = [];
+
+updateLocalCart();
+
+function loadProducts(){
+    tbody.innerHTML = "";
+
+    productsInCart.forEach(product => {
+        let tr = createNode('tr');
+        let td1 = createNode('td');
+        let td2 = createNode('td');
+        let td3 = createNode('td');
+        let td4 = createNode('td');
+        let td5 = createNode('td');
+
+        td1.innerHTML = product.name;
+        td2.innerHTML = `<img src="${product.image}" alt="Produktbild">`;
+        td3.innerHTML = `<strong>${product.price}kr</strong>`;
+        td4.innerHTML = product.quantity;
+        td5.innerHTML = `<button class="btn btn-outline-danger" onclick="removeFromCart(${product})"><strong>–</strong></button>
+                         <button class="btn btn-outline-success ms-3" onclick="addToCart(${product})"><strong>+</strong></button>`
+
+        append(tr, td1);
+        append(tr, td2);
+        append(tr, td3);
+        append(tr, td4);
+        append(tr, td5);
+        append(tbody, tr);
+    });
+}
+
+function addToCart(product){
+    alert("added");
+    
+    updateLocalCart();
+}
+
+function removeFromCart(product){
+    alert("removed");
+    console.log("hej");
+}
+
+function emptyCart(){
+    localStorage.removeItem('productsInCart');
+    updateLocalCart();
+}
+
+/**
+ * Updates whenever a product is added or removed
+ */
+function updateLocalCart(){
+
+    productsInCart = JSON.parse(localStorage.getItem('productsInCart'));
+    loadProducts();
+}
