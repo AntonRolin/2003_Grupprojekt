fb760cd6a09b1ccad9d136758ae0be9ed4d143ac
Sako05 <salem-k@hotmail.com>
2021-04-06 10:46:19 +0200

    .

--- signup.js
+++ signup.js
@@ -28,7 +28,7 @@ function checkInputs() {
   const emailValue = email.value.trim();
   const addressValue = address.value.trim();
   const zipcodeValue = zipcode.value.trim();
-  const cityValue = zipcode.value.trim();
+  const cityValue = city.value.trim();
   const phonenrValue = phonenr.value.trim();
   const password1Value = password1.value.trim();
   const password2Value = password2.value.trim();
@@ -164,22 +164,22 @@ function isEmail(email) {
  * Validates zipcode with regex
  */
 function isZipcode(zipcode) {
-	return  /^\d{3}\d{2}$/.test(zipcode);
+	return /^\d{3}\d{2}$/.test(zipcode);
 }
 
 /**
  * Validates phonenr with regex
  */
 function isPhoneNr(phonenr) {
-	return  /^(([+]46)\s*(7)|07)[02369]\s*(\d{4})\s*(\d{3})$/.test(phonenr);
+	return /^(([+]46)\s*(7)|07)[02369]\s*(\d{4})\s*(\d{3})$/.test(phonenr);
 }
 
 function isPassword(password1) {
-	return  /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password1);
+	return /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password1);
 }
 
 function isLetters(city) {
-	return  /^[a-öA-Ö]+$/.test(city);
+	return /^[a-öA-Ö]+$/.test(city);
 }
 
 function checkUserInfo(email){
