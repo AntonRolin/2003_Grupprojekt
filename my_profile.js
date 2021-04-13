02e66247044189e651777ba2f741246c8b439231
Anton Rolin <antonrolin@hotmail.com>
2021-04-13 13:33:51 +0200

    frontend complete

--- my_profile.js
+++ my_profile.js
@@ -8,19 +8,26 @@ const zipcode = document.getElementById('zipcodeCell');
 const passwordForm = document.getElementById('passwordForm');
 const profileButtons = document.getElementById('profileButtons');
 const saveProfile = document.getElementById('saveProfile');
+const errorText = document.getElementById('errorText');
+const successText = document.getElementById('successText');
 
 let user;
+user = JSON.parse(localStorage.getItem('user'));
 
 checkIfLoggedIn();
 getUserInfo();
 
+/**
+ * Checks if user is logged in, if not the site redirects to index
+ */
 function checkIfLoggedIn(){
     if(localStorage.getItem('user') == undefined)
         window.location.href = 'index.html';
 }
-
+/**
+ * Updates table with the logged in users info from localStorage
+ */
 function getUserInfo(){
-    user = JSON.parse(localStorage.getItem('user'));
 
     firstName.innerHTML = `${user.firstname}`;
     lastName.innerHTML = `${user.lastname}`;
@@ -29,26 +36,37 @@ function getUserInfo(){
     city.innerHTML = `${user.city}`;
     zipcode.innerHTML = `${user.zipcode}`;
 }
-
+/**
+ * When you press change user info button
+ */
 function changeUserInfo(){ 
+    successText.innerHTML = "";
+    errorText.innerHTML = "";
     profileButtons.style.display = 'none';
 
-    firstName.innerHTML = `<input type="text" class="form-control form-control-sm" placeholder="${user.firstname}">`;
-    lastName.innerHTML = `<input type="text" class="form-control form-control-sm" placeholder="${user.lastname}">`;
-    email.innerHTML = `<input type="text" class="form-control form-control-sm" placeholder="${user.email}">`;
-    shipping.innerHTML = `<input type="text" class="form-control form-control-sm" placeholder="${user.shipping}">`;
-    city.innerHTML = `<input type="text" class="form-control form-control-sm" placeholder="${user.city}">`;
-    zipcode.innerHTML = `<input type="text" class="form-control form-control-sm" placeholder="${user.zipcode}">`;
+    firstName.innerHTML = `<input type="text" class="form-control form-control-sm" id="userInfoFirstname" placeholder="${user.firstname}">`;
+    lastName.innerHTML = `<input type="text" class="form-control form-control-sm" id="userInfoLastname" placeholder="${user.lastname}">`;
+    email.innerHTML = `<input type="text" class="form-control form-control-sm" id="userInfoEmail" placeholder="${user.email}">`;
+    shipping.innerHTML = `<input type="text" class="form-control form-control-sm" id="userInfoShipping" placeholder="${user.shipping}">`;
+    city.innerHTML = `<input type="text" class="form-control form-control-sm" id="userInfoCity" placeholder="${user.city}">`;
+    zipcode.innerHTML = `<input type="text" class="form-control form-control-sm" id="userInfoZipcode" placeholder="${user.zipcode}">`;
 
     saveProfile.innerHTML = `<button type="submit" class="btn btn-outline-success mt-3 float-end" onclick="saveUserInfo()">Spara ändringar</button>`
 }
-
+/**
+ * When you press log out button
+ */
 function logOut(){
     localStorage.removeItem('user');
     window.location.href = 'index.html';
 }
 
+/**
+ * When you press change password button
+ */
 function changePassword(){
+    successText.innerHTML = "";
+    errorText.innerHTML = "";
     profileButtons.style.display = 'none';
 
     passwordForm.innerHTML = `<form>
@@ -64,21 +82,64 @@ function changePassword(){
         <label for="newPassword2" class="form-label">Bekräfta nytt lösenord</label>
         <input type="password" class="form-control" id="newPassword2">
       </div>
-    <button type="submit" class="btn btn-outline-success float-end" onclick="savePassword()">Spara ändringar</button>
+    <button type="button" class="btn btn-outline-success float-end" onclick="savePassword()">Spara ändringar</button>
   </form>`;
 }
-
+/**
+ * When you press the save password change button
+ */
 function savePassword(){
+
     let currentPassword = document.getElementById('currentPassword');
     let newPassword = document.getElementById('newPassword');
     let newPassword2 = document.getElementById('newPassword2');
 
-    if(newPassword.value == newPassword2.value){
-        console.log("samma");
-    }
+    if(currentPassword.value != user.password)
+        errorText.innerHTML = "Ditt nuvarande lösenord stämmer inte. Försök igen";
+    else{
+        if(newPassword.value != newPassword2.value){
+            errorText.innerHTML = "Ditt nya lösenord stämmer inte överrens. Försök igen";
+        }
+        else{
+          if(newPassword.value.length == 0)
+              errorText.innerHTML = "Ditt nya lösenord måste vara längre än 0 tecken";
+          else{
+          successText.innerHTML = "Ditt lösenord har ändrats";
+          user.password = newPassword.value;
+        }
+      }
+  }
+    profileButtons.style.display = 'block';
+    passwordForm.innerHTML = "";
+    getUserInfo();
 }
-
+/**
+ * When you press the save user info button
+ */
 function saveUserInfo(){
+
+    let firstname = document.getElementById('userInfoFirstname');
+    let lastname = document.getElementById('userInfoLastname');
+    let email = document.getElementById('userInfoEmail');
+    let shipping = document.getElementById('userInfoShipping');
+    let city = document.getElementById('userInfoCity');
+    let zipcode = document.getElementById('userInfoZipcode');
+
+    if(firstname.value != "")
+      user.firstname = firstname.value;
+    if(lastname.value != "")
+      user.lastname = lastname.value;
+    if(email.value != "")
+      user.email = email.value;
+    if(shipping.value != "")
+      user.shipping = shipping.value;
+    if(city.value != "")
+      user.city = city.value;
+    if(zipcode.value != "")
+      user.zipcode = zipcode.value;
+
+    successText.innerHTML = "Dina uppgifter har sparats";
+
     profileButtons.style.display = 'block';
     getUserInfo();
     saveProfile.innerHTML = "";
